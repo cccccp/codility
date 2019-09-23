e05a722f37da880ba1a8fec4a8f4549dbe8c1a48
cccccp <cccccp@hotmail.com>
2019-09-23 17:06:05 +0200

    Update flood_depth.py

--- flood_depth.py
+++ flood_depth.py
@@ -1,1 +1,17 @@
-
+def solution(A): #O(n) time/space complexity
+    n=len(A)
+	# Define the lists of nearest left/right summits (depending on the position i)
+    max_left_heights=[A[0]]
+    max_right_heights=[A[-1]]
+    
+    for i in range(1,n):
+        max_left_heights.append(max(A[i],max_left_heights[-1]))
+        
+    for i in range(n-2,-1,-1):
+        max_right_heights=[max(A[i],max_right_heights[0])]+max_right_heights
+        
+    depth=0
+    for i in range(n):
+        depth=max(depth, min(max_right_heights[i],max_left_heights[i])-A[i])
+        
+    return depth
